255cb51eddbd267941192693e7a9a088ee45a664
v1rg1nf0r3v3r <egorkasuslov19@gmail.com>
2022-09-19 13:32:42 +0500

    Добавьте файлы проекта.

---
+++ PP/Model/Object.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PP.Model
+{
+    public class Objectz
+    {
+        public string Date { get; set; }
+        public string Name { get; set; }
+
+        public static Objectz[] GetObjects()
+        {
+            var result = new Objectz[]
+            {
+            new Objectz() { Date = "19.12.12", Name = "Камера 1" },
+            new Objectz() { Date = "16.12.12", Name = "Камера 2" },
+            new Objectz() { Date = "11.12.12", Name = "Камера 3" },
+            new Objectz() { Date = "10.12.12", Name = "Камера 4" },
+            new Objectz() { Date = "11.12.12", Name = "Камера 5" },
+            new Objectz() { Date = "16.12.12", Name = "Камера 6" },
+            new Objectz() { Date = "15.12.12", Name = "Камера 7" }
+            };
+            return result;
+        }
+    }
+}
+
---
+++ PP/ViewModel/ObjectViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PP.ViewModel
+{
+    public class ObjectViewModel : DependencyObject
+    {
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(ObjectViewModel), new PropertyMetadata(null));
+
+        public ObjectViewModel()
+        {
+            Items = CollectionViewSource.GetDefaultView(Model.Objectz.GetObjects());
+        }
+    }
+}
